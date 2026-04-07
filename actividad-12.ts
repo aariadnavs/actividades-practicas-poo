a705b0d90036079bf9ae99a1091f4c33f9b9916e
aariadnavs <ariadnavsantillan@gmail.com>
2026-04-07 19:51:28 -0300

    Actividad 12 y 13

---
+++ actividad-12.ts
@@ -0,0 +1,5 @@
+let n1: number = 1;
+let n2: number = 2;
+let n3: number = 3;
+
+console.log(n1, n2, n3);
---
+++ actividad-13.ts
@@ -0,0 +1,3 @@
+let z: number = 1; let x: number = 2; let y: number = 3;
+
+console.log(z, x, y);
